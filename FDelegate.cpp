fd5f6105e978999b0b62ac5369c88cab14f90ae5
Benjaminshuzhiming <1326049058@qq.com>
2022-11-21 11:18:56 +0800

    去掉一些杂项

--- FDelegate.cpp
+++ FDelegate.cpp
@@ -1,40 +1,36 @@
-#include "stdafx.h"
 #include "FDelegate.h"
 
-namespace FantacyUI
+class FGlobalDelegate : public IDelegate
 {
-	class FGlobalDelegate : public IDelegate
-	{
-		friend IDelegate* MakeDelegate(GlobalCallbackFunc callback);
-	protected:
-		FGlobalDelegate(GlobalCallbackFunc func);
-		~FGlobalDelegate() {}
-	public:
-		virtual void Call(void* args);
+	friend IDelegate* MakeDelegate(GlobalCallbackFunc callback);
+protected:
+	FGlobalDelegate(GlobalCallbackFunc func);
+	~FGlobalDelegate() {}
+public:
+	virtual void Call(void* args);
 
-		virtual void Release();
-	private:
-		GlobalCallbackFunc m_func;
-	};
+	virtual void Release();
+private:
+	GlobalCallbackFunc m_func;
+};
 
-}
 
-FantacyUI::FGlobalDelegate::FGlobalDelegate(GlobalCallbackFunc func)
+FGlobalDelegate::FGlobalDelegate(GlobalCallbackFunc func)
 	: m_func(func)
 {
 }
 
-void FantacyUI::FGlobalDelegate::Call(void * args)
+void FGlobalDelegate::Call(void * args)
 {
 	m_func(args);
 }
 
-void FantacyUI::FGlobalDelegate::Release()
+void FGlobalDelegate::Release()
 {
 	delete this;
 }
 
-FantacyUI::IDelegate * FantacyUI::MakeDelegate(GlobalCallbackFunc callback)
+IDelegate * MakeDelegate(GlobalCallbackFunc callback)
 {
 	FGlobalDelegate* globalDelegate = new FGlobalDelegate(callback);
 	return reinterpret_cast<IDelegate*>(globalDelegate);
--- FDelegate.h
+++ FDelegate.h
@@ -1,57 +1,54 @@
 
 
-#ifndef __FANTACY_DELEGATE_H__
-#define __FANTACY_DELEGATE_H__
-namespace FantacyUI
+#ifndef __DELEGATE_H__
+#define __DELEGATE_H__
+class IDelegate
 {
-	class IDelegate
-	{
-	public:
-		//called callback function.
-		virtual void Call(void* args = nullptr) = 0;
+public:
+	//called callback function.
+	virtual void Call(void* args = nullptr) = 0;
 
-		virtual void Release() = 0;
-	};
+	virtual void Release() = 0;
+};
 
+template<class TClass>
+class FDelegate : public IDelegate
+{
+	typedef void(TClass::*CallbackFunc)(void* args);
 	template<class TClass>
-	class FDelegate : public IDelegate
+	friend IDelegate* MakeDelegate(TClass* pClass, void(TClass::*CallbackFunc)(void*));
+protected:
+	
+	FDelegate(TClass* pClass, CallbackFunc func)
 	{
-		typedef void(TClass::*CallbackFunc)(void* args);
-		template<class TClass>
-		friend IDelegate* MakeDelegate(TClass* pClass, void(TClass::*CallbackFunc)(void*));
-	protected:
-		
-		FDelegate(TClass* pClass, CallbackFunc func)
-		{
-			this->Obj = pClass;
-			this->Callback = func;
-		}
-	public:
-		virtual void Call(void* args)
-		{
-			(this->Obj->*Callback)(args);
-		}
-
-		virtual void Release()
-		{
-			delete this;
-		}
-	private:
-		TClass* Obj;
-		CallbackFunc Callback;
-	};
-
-	template<class TClass>
-	IDelegate* MakeDelegate(TClass* pClass, void(TClass::*CallbackFunc)(void*))
+		this->Obj = pClass;
+		this->Callback = func;
+	}
+public:
+	virtual void Call(void* args)
 	{
-		FDelegate<TClass>* pDelegate = new FDelegate<TClass>(pClass, CallbackFunc);
-		return reinterpret_cast<IDelegate*>(pDelegate);
+		(this->Obj->*Callback)(args);
 	}
 
-	typedef void(*GlobalCallbackFunc)(void* args);
-	IDelegate* MakeDelegate(GlobalCallbackFunc callback);
-}	//namespace FantacyUI
+	virtual void Release()
+	{
+		delete this;
+	}
+private:
+	TClass* Obj;
+	CallbackFunc Callback;
+};
+
+template<class TClass>
+IDelegate* MakeDelegate(TClass* pClass, void(TClass::*CallbackFunc)(void*))
+{
+	FDelegate<TClass>* pDelegate = new FDelegate<TClass>(pClass, CallbackFunc);
+	return reinterpret_cast<IDelegate*>(pDelegate);
+}
+
+typedef void(*GlobalCallbackFunc)(void* args);
+IDelegate* MakeDelegate(GlobalCallbackFunc callback);
 
 
-#endif	//__FANTACY_DELEGATE_H__
+#endif	//__DELEGATE_H__
 
